c924c87a8d40a86ede62ef018755b3d46c9a98c3
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-23 16:26:32 +0300

    created a simple component UI component Card for covered styles children's components. Added some module styles for AddUser and Card component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
+
 const AddUser = (props) => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
